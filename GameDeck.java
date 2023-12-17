c42f784daa38fcd1d0b7d44ca257995bbbef7f41
sineoyku <sineoyku@gmail.com>
2023-12-17 14:20:26 +0300

    Dealing the cards
    deleted the previous shuffle method and dealt the cards.

--- GameDeck.java
+++ GameDeck.java
@@ -14,27 +14,12 @@ public class GameDeck{
    for(int i = 0; i<10; i++){ cardcolor[i]= "blue";}
    for(int i = 10; i<20; i++){ cardcolor[i] = "yellow";}
    for(int i = 20; i<30; i++){ cardcolor[i] = "red";}
-   for(int i = 30; i<40; i++){ cardcolor[i] = "green";} 
- }
- public void setCardnum(){
-   for(int i = 0; i<10; i++){
-     cardnum[i] = i+1;   }
-   for(int i = 10; i<40; i++){
-     if(i%10==0) cardnum[i]= 1;	   
-       else cardnum[i] = i%10+1;  }
- }
- public void setCardnum(int a, int b){cardnum[a] = cardnum[b]; }
- public void setCardcolor(){
-   for(int i = 0; i<10; i++){ cardcolor[i]= "blue";}
-   for(int i = 10; i<20; i++){ cardcolor[i] = "yellow";}
-   for(int i = 20; i<30; i++){ cardcolor[i] = "red";}
-   for(int i = 30; i<40; i++){ cardcolor[i] = "green";}
-  }
+   for(int i = 30; i<40; i++){ cardcolor[i] = "green";}  }
+ 
+ public void setCardnum(int a, int b){cardnum[a] = b; }
  public void setCardcolor(int a, String b){ cardcolor[a]=b; }
- public int[] getCardnumarr(){return cardnum;}
- public String[] getCardcolorarr(){return cardcolor;}
+
  public int getCardnum(int x){return cardnum[x];}
  public String getCardcolor(int x){return cardcolor[x];}
- 
- 
+  
 }
--- GameDemo.java
+++ GameDemo.java
@@ -1,102 +1,131 @@
 import java.util.Random;
 
 public class GameDemo{
-  public static boolean includes(int[] arr, int x){
-    int k = arr.length;
-    boolean bool = true;
-    for(int i = 0; i<k; i++){
-      if(arr[i] == x) bool = true;
-      else bool= false;   }
-    return bool;
-  }
-
- public static boolean contains(String[] arr, String x){
-    int k = arr.length;
-    boolean bool = true;
-    for(int i = 0; i<k; i++){
-      if(arr[i] == x) bool = true;
-      else bool= false;    }
-    return bool;
-  }
 
  public static void Shuffle(GameDeck k, int x){
    Random rd = new Random();
+
    int z = 0;
    int y = 0;   
+   int tempnum ;
+   String tempcolor ;
+
    for(int i = 0; i<x; i++){
        z = rd.nextInt(40);
        y = rd.nextInt(40);
-      
-      int temp = k.getCardnum(z);
-      k.setCardnum(z,y);
-      k.setCardnum(y, temp);
 
-      String temp2 = k.getCardcolor(z);
+      tempcolor = k.getCardcolor(z);
       k.setCardcolor(z, k.getCardcolor(y));
-      k.setCardcolor(y, temp2);
+      k.setCardcolor(y, tempcolor);
 
+
+      tempnum = k.getCardnum(z);
+      k.setCardnum(z, k.getCardnum(y));
+      k.setCardnum(y, tempnum);
    }
  }
-
-/*
- public static void Shuffle(GameDeck k){
-   Random rd = new Random();
-
-   int[] number = new int[40];
-   for(int i = 0; i<number.length; i++){
-       number[i] = k.getCardnum(i);    }
-   String[] color = new String[40];
-   for(int i = 0; i<color.length; i++){
-       color[i] = k.getCardcolor(i); }
- 
-
-   for(int i = 20; i>0; i--){
-
-	 int x = 0; 
-	 while(true){
-	     x = rd.nextInt(number.length); 
-	    if(includes(number, x) == true && contains(color, k.getCardcolor(x)) == true){ break;} }
-	 
-	 int y = 0;        
-	 while(true){
-	      y = rd.nextInt(number.length);
-	     if(includes(number, y) == true && contains(color, k.getCardcolor(y)) == true){ break;}}
-
-         if(x==y){
-	 while(true){
-	     x = rd.nextInt(number.length); 
-	    if(includes(number, x) == true && contains(color, k.getCardcolor(x)) == true) {break; }}
-	         
-	 while(true){
-	      y = rd.nextInt(number.length);
-	   if(includes(number, y) == true && contains(color, k.getCardcolor(y)) == true){ break;} }}
-	 
-
-         int tempnum = k.getCardnum(x);
-	 k.setCardnum(x,y);
-       	 k.setCardnum(y, tempnum);
-
-	 String tempcolor = k.getCardcolor(x);
-         k.setCardcolor(x, k.getCardcolor(y));
-	 k.setCardcolor(y, tempcolor);
-
-         
-         for(int j = 0; j<40; j++){
-            if(j == x || j == y){
-	       number[j]=-1;
-	       color[j] = "null";}
-            else number[j] = k.getCardnum(j);    } 
-
-	 
-   }
- }
-*/	
   public static void main(String[] args){
+    Random rd = new Random();	  
     GameDeck x = new GameDeck();
-    Shuffle(x, 16);
-       for(int i = 0; i<40; i++){
+    Shuffle(x, 50);
+
+    // test gamedeck
+    System.out.println("Game Deck: ");	
+    for(int i = 0; i<40; i++){
       System.out.println(x.getCardcolor(i) + x.getCardnum(i)); 
+      } 
+
+    int[] plNum = new int[10];
+          //player's card deck's numbers
+    String[] plColor = new String[10];
+         //player's card deck's colors
+    for(int i = 0; i<5; i++){
+      plNum[i] = x.getCardnum(i);
+      plColor[i] = x.getCardcolor(i);
+      }
+
+    int temp;
+    String color;   
+    int sign;
+
+    for(int i = 0; i<3; i++){
+        temp = rd.nextInt(40);
+        color = x.getCardcolor(temp);
+        plColor[i+5] = color;
+        temp = rd.nextInt(6) + 1;
+	sign = rd.nextInt(2);
+	if(sign == 0)  plNum[i+5] = temp;
+	else plNum[i+5] = (-1)*temp;
     }
+    int chance = rd.nextInt(10);
+    if(chance<=8){
+      for(int i = 0; i<2; i++){
+        temp = rd.nextInt(40);
+        color = x.getCardcolor(temp);
+        plColor[i+8] = color;
+        temp = rd.nextInt(6) + 1;
+	sign = rd.nextInt(2);
+	if(sign == 0)  plNum[i+8] = temp;
+	else plNum[i+8] = (-1)*temp;
+      }
+    }
+    else{
+        plColor[8] = "flip card";
+	plNum[8] = -1 ;
+	plColor[9] = "double card";
+	plNum[9] = -2; 
+    }
+    
+
+    int[] cpNum = new int[10];
+    String[] cpColor = new String[10];
+    int j = 0;
+    for(int i = 39; i>34; i--){	    
+       cpNum[j]= x.getCardnum(i);
+       cpColor[j]= x.getCardcolor(i);
+       j++;
+       }
+    for(int i = 0; i<3; i++){
+        temp = rd.nextInt(40);
+        color = x.getCardcolor(temp);
+        cpColor[i+5] = color;
+        temp = rd.nextInt(6) + 1;
+	sign = rd.nextInt(1);
+	if(sign == 0)  cpNum[i+5] = temp;
+	else cpNum[i+5] = (-1)*temp;
+   }
+    chance = rd.nextInt(10);
+    if(chance<=8){
+      for(int i = 0; i<2; i++){
+        temp = rd.nextInt(40);
+        color = x.getCardcolor(temp);
+        cpColor[i+8] = color;
+        temp = rd.nextInt(6) + 1;
+	sign = rd.nextInt(2);
+	if(sign == 0)  cpNum[i+8] = temp;
+	else cpNum[i+8] = (-1)*temp;
+      }
+    }
+    else{
+        cpColor[8] = "flip card";
+	cpNum[8] = -1 ;
+	cpColor[9] = "double card";
+	cpNum[9] = -2; 
+    }
+
+    //test player decks
+   System.out.println("PLAYER HAND");
+   for(int i = 0; i<10; i++){
+      System.out.println(plColor[i] +" "+ plNum[i]);
+   }
+   System.out.println("COMPUTER HAND");
+   for(int i = 0; i<10; i++){
+     System.out.println(cpColor[i] +" "+ cpNum[i]);
+   }
+
+
+    
+
     
   }
 }
